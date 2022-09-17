fb955a2d6216c5acfa4178f3265d9f7e02f801f2
Gmishra2000 <mantumisara485@gmail.com>
2022-09-18 02:59:30 +0530

    3rd-challenge Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  firststep: boolean;
+  count: number = 0;
+  buttonLog = [];
+
+  toggleParagraph() {
+    if (this.firststep) {
+      this.firststep = false;
+    } else {
+      this.firststep = true;
+    }
+    this.count = this.count + 1;
+    this.buttonLog.push(this.count);
+  }
 }
